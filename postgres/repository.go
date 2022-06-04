a7e724981c2063248c7bfad05dac4bb2a2ac1a62
Your Name <you@example.com>
2022-06-04 10:25:36 +0100

    revert original sql statements

--- postgres/repository.go
+++ postgres/repository.go
@@ -21,14 +21,14 @@ func NewRepo(db *sql.DB) *BookingRepo {
 
 // Insert satisfies Repository.
 func (r *BookingRepo) Insert(ctx context.Context, b innsecure.Booking) error {
-	stmt := fmt.Sprintf(`insert into "Bookings`)
+	stmt := fmt.Sprintf(`insert into "Bookings" ("id", "hotelid", "arrive", "leave", "name") values ('%s', '%d', '%s', '%s', '%s')`, b.ID, b.HotelID, b.Arrive, b.Leave, b.Name)
 	_, err := r.db.Exec(stmt)
 	return err
 }
 
 // List returns the full contents of the repository.
 func (r *BookingRepo) List(ctx context.Context, hotelID int) ([]innsecure.Booking, error) {
-	stmt := fmt.Sprintf(`insert into "Bookings`)
+	stmt := fmt.Sprintf(`select "id", "hotelid", "arrive", "leave", "name" from "Bookings" WHERE hotelid='%d'`, hotelID)
 	rows, err := r.db.Query(stmt)
 	if err != nil {
 		return nil, fmt.Errorf("failed to list bookings: %w", err)
@@ -49,7 +49,7 @@ func (r *BookingRepo) List(ctx context.Context, hotelID int) ([]innsecure.Bookin
 // ByID returns a single booking by ID.
 // If no booking is found with the given ID, no error is returned.
 func (r *BookingRepo) ByID(ctx context.Context, hotelID int, ID string) (*innsecure.Booking, error) {
-	q := fmt.Sprintf(`select "id", "hotelid", "arrive", "leave", "name" from "Bookings" WHERE hotelid='%d'`, hotelID)
+	q := fmt.Sprintf(`select "id", "hotelid", "arrive", "leave", "name" from "Bookings" where "hotelid"='%d' AND "id"='%s'`, hotelID, ID)
 	rows, err := r.db.Query(q)
 	if err != nil {
 		return nil, err
